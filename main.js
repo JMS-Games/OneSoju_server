a114d26fcc1c28a2ba7ebd826fbd250ddd9715ff
taegyunhan <taegyun.han@playlinks.com>
2022-11-15 14:23:07 +0900

    feat: add socket.js

--- main.js
+++ main.js
@@ -1,47 +1,12 @@
 const app = require('express')();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
+const SocketManager = require('./src/socket').getInstance(io);
 
-const SIG = require('./src/signal');
 const CONFIG = require('./src/config');
-const CODE = require('./src/code');
-
-const GM = require('./src/gamemanager').getInstance();
-const Player = require('./src/player');
-
 
 server.listen(CONFIG.PORT, () => {
     console.log(`server is listening on ${CONFIG.PORT}`);
 });
 
-
-io.on('connection', (socket) => {
-    let curPlayer = null;
-
-    socket.on('disconnect', () => {
-        console.log(`client disconnected from socket id: ${socket.id}, player uuid: ${curPlayer.uuid}`);
-        curPlayer && GM.removePlayer(curPlayer);
-    });
-
-    socket.on(SIG.REQUEST_MATCH, (req, res) => {
-        curPlayer = new Player(socket.id, req.uuid);
-        GM.addPlayer(curPlayer, res);
-    });
-
-    socket.on(SIG.START_GAME, (req, res) => {
-        GM.startGame(curPlayer, SIG.START_GAME, res, io);
-    });
-
-    socket.on(SIG.JOIN_ROOM, (req, res) => {
-        GM.broadcastRoom(curPlayer, SIG.JOIN_ROOM, {CODE: CODE.OK, msg: `player ${curPlayer.uuid} joined!`}, io);
-    });
-
-    socket.on(SIG.EXIT_ROOM, (req, res) => {
-        GM.broadcastRoom(curPlayer, SIG.EXIT_ROOM, {CODE: CODE.OK, msg: `player ${curPlayer.uuid} left the room.`}, io);
-    });
-
-    socket.on(SIG.USE_CARD, (req, res) => {
-        GM.playCard(curPlayer, req.nextCard, res);
-    });
-
-});
+SocketManager.boot();
---
+++ src/socket.js
@@ -0,0 +1,65 @@
+const SIG = require('./signal');
+const CODE = require('./code');
+
+const Player = require('./player');
+const GM = require('./gamemanager').getInstance();
+
+
+const SocketManager = (function() {
+    let instance;
+
+    function init(io_) {
+        return {
+            getIo: function () {
+                return this.io;
+            },
+
+            boot: function () {
+                this.io.on('connection', (socket) => {
+                    let curPlayer = null;
+
+                    socket.on('disconnect', () => {
+                        console.log(`client disconnected from socket id: ${socket.id}, player uuid: ${curPlayer.uuid}`);
+                        curPlayer && GM.removePlayer(curPlayer);
+                    });
+
+                    socket.on(SIG.REQUEST_MATCH, (req, res) => {
+                        curPlayer = new Player(socket.id, req.uuid);
+                        GM.addPlayer(curPlayer, res);
+                    });
+
+                    socket.on(SIG.START_GAME, (req, res) => {
+                        GM.startGame(curPlayer, SIG.START_GAME, res, this.io);
+                    });
+
+                    socket.on(SIG.JOIN_ROOM, (req, res) => {
+                        GM.broadcastRoom(curPlayer, SIG.JOIN_ROOM, {CODE: CODE.OK, msg: `player ${curPlayer.uuid} joined!`}, this.io);
+                    });
+
+                    socket.on(SIG.EXIT_ROOM, (req, res) => {
+                        GM.broadcastRoom(curPlayer, SIG.EXIT_ROOM, {CODE: CODE.OK, msg: `player ${curPlayer.uuid} left the room.`}, this.io);
+                    });
+
+                    socket.on(SIG.USE_CARD, (req, res) => {
+                        GM.playCard(curPlayer, req.nextCard, res);
+                    });
+
+                });
+            },
+
+            io: io_
+        }
+    }
+
+    return {
+        getInstance: function(io_) {
+            if (instance) {
+                return instance;
+            }
+            instance = init(io_);
+            return instance;
+        }
+    }
+})();
+
+module.exports = SocketManager;
