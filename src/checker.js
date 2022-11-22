05ccfc94823f68879c67a74a8d2f473a285cb4a7
taegyunhan <taegyun.han@playlinks.com>
2022-11-22 11:21:25 +0900

    feat: add checker func detail

--- src/checker.js
+++ src/checker.js
@@ -12,12 +12,27 @@ class Checker {
             return true;
         }
 
-        // not match with shape or number
-        if (!(curCard.shape === nextCard.shape || curCard.value === nextCard.value)) {
+        // when current card is joker
+        if (curCard.value === CONFIG.CARD_VALUE.JOKER) {
+            const shape = curCard.shape;
+            if (shape === "DIAMOND" && (nextCard.shape === 'SPADE' || nextCard.shape === 'CLOVER')) {
+                return true;
+            }
+
+            if (shape === "SPADE" && (nextCard.shape === 'DIAMOND' || nextCard.shape === 'HEART')) {
+                return true;
+            }
+
+            return !(nextCard.value === CONFIG.CARD_VALUE["3"] || nextCard.value === CONFIG.CARD_VALUE.JOKER);
+        }
+
+        // if atk is lower than cur card.
+        if (nextCard.atk < curCard.atk) {
             return true;
         }
 
-        return false;
+        // not match with shape or number
+        return !(curCard.shape === nextCard.shape || curCard.value === nextCard.value);
     }
 }
 
