a6ec32e1e85804fd101647cc081afb358093e592
joon <minjoon.song@playlinks.com>
2023-02-07 18:17:28 +0900

    [fix] signal.js string 타입으로 수정

--- src/signal.js
+++ src/signal.js
@@ -1,16 +1,16 @@
 const SIG = {
     // client side
-    REQUEST_MATCH: 1,
-    START_GAME: 2,
-    DRAW_CARD: 3,
-    USE_CARD: 4,
+    REQUEST_MATCH: "1",
+    START_GAME: "2",
+    DRAW_CARD: "3",
+    USE_CARD: "4",
 
     // server side
-    JOIN_ROOM: 100,
-    EXIT_ROOM: 200,
-    YOUR_TURN: 300,
-    USE_RESULT: 400,
-    SOMEONE_WIN: 500
+    JOIN_ROOM: "100",
+    EXIT_ROOM: "200",
+    YOUR_TURN: "300",
+    USE_RESULT: "400",
+    SOMEONE_WIN: "500"
 };
 
 Object.freeze(SIG);
