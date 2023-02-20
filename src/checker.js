ca35196db2fb6e018da99552b9675da87a87f819
taegyunhan <taegyun.han@playlinks.com>
2023-02-20 15:01:24 +0900

    fix: fix checker

--- src/checker.js
+++ src/checker.js
@@ -9,34 +9,42 @@ class Checker {
         return player.isTurn;
     }
 
-    isIllegal(curCard, nextCard, hand) {
+    isIllegal(curCard, nextCard, hand, underAttack) {
         // nextCard isn't in your hand
         if (!hand.find((element) => element.id === nextCard.id)) {
             console.log('[Checker][ERROR] request error! hand: 404');
             return true;
         }
+        if (underAttack) {
+            // when current card is joker
+            if (curCard.value === CONFIG.CARD_VALUE.JOKER) {
+                const shape = curCard.shape;
+                if (shape === "DIAMOND" && (nextCard.shape === 'SPADE' || nextCard.shape === 'CLOVER')) {
+                    return true;
+                }
 
-        // when current card is joker
-        if (curCard.value === CONFIG.CARD_VALUE.JOKER) {
-            const shape = curCard.shape;
-            if (shape === "DIAMOND" && (nextCard.shape === 'SPADE' || nextCard.shape === 'CLOVER')) {
-                return true;
+                if (shape === "SPADE" && (nextCard.shape === 'DIAMOND' || nextCard.shape === 'HEART')) {
+                    return true;
+                }
+
+                return !(nextCard.value === CONFIG.CARD_VALUE["3"] || nextCard.value === CONFIG.CARD_VALUE.JOKER);
             }
 
-            if (shape === "SPADE" && (nextCard.shape === 'DIAMOND' || nextCard.shape === 'HEART')) {
+            // if atk is lower than cur card.
+            if (nextCard.atk < curCard.atk) {
                 return true;
             }
-
-            return !(nextCard.value === CONFIG.CARD_VALUE["3"] || nextCard.value === CONFIG.CARD_VALUE.JOKER);
+        } else {
+            if (curCard.value === CONFIG.CARD_VALUE.JOKER) {
+                if (curCard.shape === "DIAMOND") {
+                    return (nextCard.shape === "SPADE" || nextCard.shape === "CLOVER");
+                } else {
+                    return (nextCard.shape === "DIAMOND" || nextCard.shape === "HEART");
+                }
+            }
+            // not match with shape or number
+            return !(curCard.shape === nextCard.shape || curCard.value === nextCard.value);
         }
-
-        // if atk is lower than cur card.
-        if (nextCard.atk < curCard.atk) {
-            return true;
-        }
-
-        // not match with shape or number
-        return !(curCard.shape === nextCard.shape || curCard.value === nextCard.value);
     }
 
     getCandidateCards(player, room) {
